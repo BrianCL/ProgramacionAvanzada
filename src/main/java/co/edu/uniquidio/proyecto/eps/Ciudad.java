f311cb98f37859fc7b9850c93160f667b5e7d1cd
BrianCL <brianloaizac@gmail.com>
2023-09-12 11:32:05 -0500

    Creacion del proyecto

---
+++ src/main/java/co/edu/uniquidio/proyecto/eps/Ciudad.java
@@ -0,0 +1,24 @@
+package co.edu.uniquidio.proyecto.eps;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Ciudad implements Serializable {
+    @Id
+    @EqualsAndHashCode.Include
+    private String id_eps;
+    private String nombre;
+
+
+}
---
+++ src/main/java/co/edu/uniquidio/proyecto/eps/Usuario.java
@@ -0,0 +1,26 @@
+package co.edu.uniquidio.proyecto.eps;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Usuario implements Serializable {
+    @Id
+    @EqualsAndHashCode.Include
+    private String id_usuario;
+    private String contrasena;
+    private String email;
+    private String tipo_usuario;
+
+
+}
